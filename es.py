d63b283b05f3bad8ea2f19b37220f80f50b85e6c
Justin P. Slattery <jpslattery1993@gmail.com>
2023-06-08 20:40:00 -0400

    Fleshing out structure of search

--- es.py
+++ es.py
@@ -24,7 +24,6 @@ class EvoSearch:
             optional keys -
                 fitness_args: list-like - optional additional arguments to pass while calling fitness function
                                            list such that len(list) == 1 or len(list) == pop_size
-                num_processes: int -  pool size for multiprocessing.pool.Pool - defaults to os.cpu_count()
         '''
         # check for required keys
         required_keys = ['pop_size','genotype_size','fitness_function','elitist_fraction','mutation_variance']
@@ -39,25 +38,34 @@ class EvoSearch:
         self.elitist_fraction = evo_params['elitist_fraction']
         self.mutation_variance = evo_params['mutation_variance']
 
-        # validating fitness function
-        assert self.fitness_function,"Invalid fitness_function"
-        rand_genotype = np.random.rand(self.genotype_size)
-        rand_genotype_fitness = self.fitness_function(rand_genotype)
-        assert type(rand_genotype_fitness) == type(0.) or type(rand_genotype_fitness) in np.sctypes['float'],\
-               "Invalid return type for fitness_function. Should be float or np.dtype('np.float*')"
+        # # validating fitness function
+        # assert self.fitness_function,"Invalid fitness_function"
+        # rand_genotype = np.random.rand(self.genotype_size)
+        # rand_genotype_fitness = self.fitness_function(rand_genotype)
+        # assert type(rand_genotype_fitness) == type(0.) or type(rand_genotype_fitness) in np.sctypes['float'],\
+        #        "Invalid return type for fitness_function. Should be float or np.dtype('np.float*')"
 
-        # create other required data
-        self.num_processes = evo_params.get('num_processes',None)
-        self.pop = np.random.rand(self.pop_size,self.genotype_size)
-        self.fitness = np.zeros(self.pop_size)
-        self.num_batches = int(self.pop_size/self.num_processes)
-        self.num_remainder = int(self.pop_size%self.num_processes)
+        # # create other required data
+        # self.num_processes = evo_params.get('num_processes',None)
+        # self.pop = np.random.rand(self.pop_size,self.genotype_size)
+        # self.fitness = np.zeros(self.pop_size)
+        # self.num_batches = int(self.pop_size/self.num_processes)
+        # self.num_remainder = int(self.pop_size%self.num_processes)
 
-        # check for fitness function kwargs
-        if 'fitness_args' in evo_params.keys():
-            optional_args = evo_params['fitness_args']
-            assert len(optional_args) == 1 or len(optional_args) == self.pop_size,\
-                    "fitness args should be length 1 or pop_size."
-            self.optional_args = optional_args
-        else:
-            self.optional_args = None
+        # # check for fitness function kwargs
+        # if 'fitness_args' in evo_params.keys():
+        #     optional_args = evo_params['fitness_args']
+        #     assert len(optional_args) == 1 or len(optional_args) == self.pop_size,\
+        #             "fitness args should be length 1 or pop_size."
+        #     self.optional_args = optional_args
+        # else:
+        #     self.optional_args = None
+
+        # Steps to fill out
+        # 1. Pull in the fitness function; identify object
+        # 2. Find most fit individual
+        # 3. If elitism is selected, slice elites
+        # 4. If mutation is selected, select and mutate agents/objects
+        # 5. Step through generations
+        # 6. Track fitness, best individuals, fitness variance, etc.
+        # 7. At gen_end, get max fitness and best individual
--- main.py
+++ main.py
@@ -4,4 +4,29 @@ Packagifying and importing evo_search classes
 Justin Slattery
 June 2023
 '''
-from evo_search.es import EvoSearch
+from es import EvoSearch
+import numpy as np
+
+# Test Array for Evo Search
+test_arr = np.zeros((2,2))
+print(test_arr)
+
+# Need to set up the functions so that they can either:
+# 1. take a preset data structure or
+# 2. take an ambiguous structure and parse it (list, np array, dict?)
+# 3. take a preset data structure with agents with genotypes
+
+
+def fitness_function(obj):
+    pass
+
+evo_params = {
+        'pop_size' : 100,    # population size
+        'genotype_size': 10, # dimensionality of solution
+        'fitness_function': fitness_function, # custom function defined to evaluate fitness of a solution
+        'elitist_fraction': 0.04, # fraction of population retained as is between generations
+        'mutation_variance': 0.05 # mutation noise added to offspring.
+    }
+
+es = EvoSearch(evo_params)
+
